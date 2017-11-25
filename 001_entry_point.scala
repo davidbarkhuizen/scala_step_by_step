76c170c543742f8d0b8ad75108926f53d645020c
david barkhuizen <david@yoco.co.za>
2017-11-25 09:59:15 +0200

    entry point

---
+++ 001_entry_point.scala
@@ -0,0 +1,23 @@
+// single line comment
+
+/*
+	multi
+	line
+	comment\
+*/
+
+/** documentation comment */
+object module {
+  
+	private def yo(sequence : Seq[Char]) : Unit = {
+
+		val str = sequence.foldLeft("")((s, c) => s + String.valueOf(c)) 
+		println(str)
+	}
+
+	def main(args: Array[String]): Unit = {   
+		
+		val charSeq = Seq('e','n','t','r','y',' ','p','o','i','n','t')
+		yo(charSeq)
+	}
+}
